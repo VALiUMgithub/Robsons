4d76ceee3aa9a4b20afd6c223451a173ec85db85
KrAman66 <22352004@pondiuni.ac.in>
2023-07-04 10:23:01 -0700

    update

--- client/src/components/generate-report-view.jsx
+++ client/src/components/generate-report-view.jsx
@@ -65,7 +65,7 @@ function GenerateReportView() {
 						</div>
 						<div className='flex flex-col justify-center mt-5'>
 							<button
-								className='bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded m-2'
+								className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded m-2'
 								onClick={() => generateReport()}
 							>
 								GENERATE REPORT
